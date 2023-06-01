8bddb1d23e15a35166fcba06f3e4313084bb3ac4
ti-mi <123125710+Timothe010@users.noreply.github.com>
2023-06-01 13:59:02 +0200

    Add files via upload

---
+++ LogicCraft/accueil.js
@@ -0,0 +1,10 @@
+let name = 'salut'; //ToString
+let New = 3;// Nomber
+
+let x = 90;
+let y = 80;
+let result = x+y;
+
+console.log('name' +' '+New);
+console.log(typeof(name));
+console.log(result);
